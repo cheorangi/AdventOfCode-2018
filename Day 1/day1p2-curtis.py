bb9f28e5b1940f7a8ea8d16972da00c1426ed8fd
Curtis Turner <cjturner@csu.fullerton.edu>
2018-12-01 15:06:44 -0800

    SPOLIER ALERT. finished day 1 part 2.

--- Day 1/day1p2-curtis.py
+++ Day 1/day1p2-curtis.py
@@ -1,11 +1,40 @@
-with open('day1p2-input.txt') as inFile:
+#read puzzle input from file
+with open('day1p2-curtis-input.txt') as inFile:
     puzzle = inFile.read().splitlines()
+
+#use this puzzle to run your test cases
+#puzzle = ['-6', '+3', '+8', '+5', '-6']
 
 #convert all strings to ints in the list of frequencies
 frequencies = [int(i) for i in puzzle]
 
-_sum = 0
+#print(frequencies)
 
+currFreq = 0
 
-for i in range(len(frequencies)):
+seen = set()
+
+cont = True
+index = 0
+
+seen.add(currFreq)
+
+while (cont):
+    if index > (len(frequencies)-1):
+        index = 0
+    #print('current value >> ' + str(frequencies[index]))
+    #print('current frequency >> ' + str(currFreq))
     
+    currFreq = currFreq + frequencies[index]
+
+    #print(seen)
+    #print('current sum >> ' + str(_sum))
+    
+
+    if currFreq in seen:
+        print(currFreq)
+        break
+
+    seen.add(currFreq)
+
+    index += 1
