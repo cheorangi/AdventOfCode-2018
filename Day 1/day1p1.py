83a846edb2217e14585934a010c959f2f7609e71
Curtis Turner <curtisturner@Curtiss-MacBook-Pro.local>
2018-12-01 09:28:12 -0800

    add comments to each line indicating what it does.

--- Day 1/day1p1.py
+++ Day 1/day1p1.py
@@ -1,7 +1,14 @@
-#supply the input as a txt file for this to work.
+#supply the input as a txt file for this to work
+#writes each line into a list where each line is an index in the list
 with open('day1p1-input.txt') as inFile:
     puzzle = inFile.read().splitlines()
 
+#convert all strings to ints in the list of frequencies
 frequencies = [int(i) for i in puzzle]
-print(frequencies)
+
+#print frequencies for debugging purposes
+# print(frequencies)
+
+#output the puzzle solution 
+# utilizing the sum function to calculate the sum of postive and negative values.
 print(sum(frequencies))
