066fe6e15a890d03db9d38c9c8c73e5d5f94d1f7
Curtis Turner <cjturner@csu.fullerton.edu>
2018-12-03 21:53:15 -0800

    partially working need to correct the total area of overlap functions

--- Day 3/day3p1-curtis.py
+++ Day 3/day3p1-curtis.py
@@ -1,19 +1,11 @@
 #getting started on day 3 this one is gonna be a doozy
-'''class Claims():
-    def __init__(self, xCoord, yCorrd, ):
-        self.x ='''
+from math import sqrt
 
-claims = ['#1 @ 1,3: 4x4', '#2 @ 3,1: 4x4', '#3 @ 5,5: 2x2']
+#claims = ['#1 @ 1,3: 4x4', '#2 @ 3,1: 4x4', '#3 @ 5,5: 2x2']
+with open('day3p1-curtis-input.txt') as infile:
+    claims = infile.read().splitlines()
 
 fabric = (1000,1000)
-
-class Point():
-    def __init__(self, x=0, y=0):
-        self.x = x
-        self.y = y
-
-class Rectangle():
-    def __init__(self, )
 
 def splitClaim(c):
     _id = c.split(' ')[0][-1]
@@ -26,17 +18,34 @@ def checkOverlap(c1, c2):
     c1Id, c1Coord, c1Size = splitClaim(c1)
     c2Id, c2Coord, c2Size = splitClaim(c2)
 
-    leftX = c1Coord[0]
-    rightX = c1Coord[0] + c1Size[0]
+    #if any part of the rectangle is between the left x and right side x
+    if c1Coord[0] > c2Coord[0] + c2Size[0] or c1Coord[0] + c1Size[0] > c2Coord[0]:
+        return False
+    
+    if c1Coord[1] < c2Coord[1] + c2Size[1] or c1Coord[1] + c1Size[1] < c2Coord[1]:
+        return False
 
-    topY = c1Coord[1]
-    bottomY = c1Coord[1] + c1Size[1]
+    return True
 
-    '''if  + c1Size[0] > c2Coord[0] + c2Size[0] or c1Coord[1] + c1Size[1] > c2Coord[1] + c2Size[1]:
-        return True'''
+def calcOverlap(c1, c2):
+    c1Id, c1Coord, c1Size = splitClaim(c1)
+    c2Id, c2Coord, c2Size = splitClaim(c2)
 
+    a1 = abs(int(c1Coord[0]) - int(c1Coord[0]) + int(c1Size[0])) * abs(int(c1Coord[1]) - int(c1Coord[1]) + int(c1Size[1]))
 
+    a2 = abs(int(c2Coord[0]) - int(c2Coord[0]) + int(c2Size[0])) * abs(int(c2Coord[1]) - int(c2Coord[1]) + int(c2Size[1]))
+
+    aI = (min(int(c1Coord[0]) + int(c1Size[0]), int(c2Coord[0]) + int(c2Size[0])) - max(int(c1Coord[0]), int(c1Coord[0]) + int(c1Size[0]))) * (min(int(c1Coord[1]) + int(c1Size[1]), int(c2Coord[1]) + int(c2Size[1])) - max(int(c1Coord[1]), int(c1Coord[1]) + int(c1Size[1])))
+
+    return (a1 + a2 - aI)
+
+totalOverlap = 0
 for i in range(len(claims)-1):
-    for j in range(i, len(claims)):
+    for j in range(i+1, len(claims)):
         if (checkOverlap(claims[i], claims[j])):
             print('Claims overlap')
+            totalOverlap += calcOverlap(claims[i], claims[j])
+        else:
+            print('No Claims Overlap')
+
+print(totalOverlap)
