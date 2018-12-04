35b3ccbad4c1c5fa7517236f00002ac7d1df2bf0
Curtis Turner <curtisturner@Curtiss-MacBook-Pro.local>
2018-12-04 13:22:57 -0800

    working on integrating a couple helper classes to make code readable

--- Day 3/day3p1-curtis.py
+++ Day 3/day3p1-curtis.py
@@ -7,13 +7,35 @@ claims = ['#1 @ 1,3: 4x4', '#2 @ 3,1: 4x4', '#3 @ 5,5: 2x2']
 
 fabric = (1000,1000)
 
+#create a few helper classes to make the code easier to read rather than using lists for all the items in the claim
 class Point():
     def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
 
 class Rectangle():
-    def __init__(self, )
+    def __init__(self, coord, width=0, height=0):
+        self.Point = coord
+        self.width = width
+        self.height = height
+
+class Claim():
+    def __init__(self, id_, rect):
+        self.id = id_
+        self.rect = rect
+
+point = Point(1, 2)
+
+print(point.x)
+
+rect = Rectangle(point, 4, 4)
+
+print(rect.width)
+
+claim = Claim(1, rect)
+
+print(claim.id)
+print(claim.rect.Point.x)
 
 def splitClaim(c):
     _id = c.split(' ')[0][-1]
