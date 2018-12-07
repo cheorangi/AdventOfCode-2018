a0bc75f7e625f3db1141e455f2bdc9d8bc06cef2
Curtis Turner <curtisturner@Curtiss-MacBook-Pro.local>
2018-12-06 17:01:00 -0800

    shortened the code a bit

--- curtis-turner/Day 1/day1p1-curtis.py
+++ curtis-turner/Day 1/day1p1-curtis.py
@@ -1,10 +1,10 @@
 #supply the input as a txt file for this to work
 #writes each line into a list where each line is an index in the list
 with open('day1p1-input.txt') as inFile:
-    puzzle = inFile.read().splitlines()
+    frequencies = [int(i) for i in inFile.read().splitlines()]
 
 #convert all strings to ints in the list of frequencies
-frequencies = [int(i) for i in puzzle]
+#frequencies = [int(i) for i in puzzle]
 
 #print frequencies for debugging purposes
 # print(frequencies)
