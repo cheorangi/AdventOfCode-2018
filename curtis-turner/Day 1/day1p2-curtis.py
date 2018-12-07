28b85bf7188733310cbe28ccc98b5b0f91a375e2
Curtis Turner <curtisturner@Curtiss-MacBook-Pro.local>
2018-12-06 20:13:40 -0800

    clean up code some.

--- curtis-turner/Day 1/day1p2-curtis.py
+++ curtis-turner/Day 1/day1p2-curtis.py
@@ -1,35 +1,24 @@
 #read puzzle input from file
 with open('day1p2-curtis-input.txt') as inFile:
-    puzzle = inFile.read().splitlines()
+    frequencies = [int(i) for i in inFile.read().splitlines()]
 
 #use this puzzle to run your test cases
 #puzzle = ['-6', '+3', '+8', '+5', '-6']
-
-#convert all strings to ints in the list of frequencies
-frequencies = [int(i) for i in puzzle]
-
-#print(frequencies)
 
 currFreq = 0
 
 seen = set()
 
+seen.add(currFreq)
+
 cont = True
 index = 0
 
-seen.add(currFreq)
-
-while (cont):
-    if index > (len(frequencies)-1):
+while cont:
+    if index > len(frequencies)-1:
         index = 0
-    #print('current value >> ' + str(frequencies[index]))
-    #print('current frequency >> ' + str(currFreq))
-    
-    currFreq = currFreq + frequencies[index]
 
-    #print(seen)
-    #print('current sum >> ' + str(_sum))
-    
+    currFreq += frequencies[index]
 
     if currFreq in seen:
         print(currFreq)
