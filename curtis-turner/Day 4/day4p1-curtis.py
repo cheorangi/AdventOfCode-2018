1eb64d5762bf0dcbd94baf9d33abacd26e26fe13
Curtis Turner <curtisturner@Curtiss-MacBook-Pro.local>
2018-12-08 10:30:13 -0800

    send updates to git

--- curtis-turner/Day 4/day4p1-curtis.py
+++ curtis-turner/Day 4/day4p1-curtis.py
@@ -1,1 +1,27 @@
+import datetime
+import re
+
 #setting up my file for day 4 if i every get there lol
+with open('day4p1-curtis-input.txt') as infile:
+    data = infile.read().splitlines()
+
+newdata =[]
+for i in range(len(data)):
+    temp = []
+    string = data[i]
+    timestamp = string.split(' ')[0] + ' ' + string.split(' ')[1]
+    timestamp = timestamp[1:-1]
+    timestap = datetime.datetime.strptime(timestamp, '%Y-%m-%d %H:%M')
+    temp.append(timestamp)
+    temp +=  ' ' + data[i]
+    newdata.append(temp)
+
+print(newdata[0])
+
+'''
+Notes in the Comment
+addtime = map(lambda l : l[0].split(' ')[0] + ' ' + l[0].split(' ')[1], data)
+print(list(addtime))
+print(data[0])
+'''
+
