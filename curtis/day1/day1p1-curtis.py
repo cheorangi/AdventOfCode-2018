ff889ba59894ec454ce74efd29d236be75f25973
curtis-turner <cjturner714@gmail.com>
2018-12-13 15:22:09 -0800

    fixed timer setup

--- curtis/day1/day1p1-curtis.py
+++ curtis/day1/day1p1-curtis.py
@@ -1,13 +1,13 @@
 #import timeit
 import time
 
-start = time.now()
+start = time.clock()
 #supply the input as a txt file for this to work
 #writes each line into a list where each line is an index in the list
 #with open('day1p1-curtis-input.txt') as inFile:
     #frequencies = [int(i) for i in inFile.read().splitlines()]
 print(sum([int(i) for i in open('day1p1-curtis-input.txt').read().splitlines()]))
-end = time.now()
+end = time.clock()
 
 print(end-start)
 #convert all strings to ints in the list of frequencies
