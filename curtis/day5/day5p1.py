a806382835d537a264bd6e5878b9db2e1d389153
Curtis Turner <curtisturner@Curtiss-MacBook-Pro.local>
2018-12-27 17:08:39 -0800

    first pass at day 5

--- curtis/day5/day5p1.py
+++ curtis/day5/day5p1.py
@@ -1,1 +1,25 @@
-#hello day 5
+#example = 'dabAcCaCBAcCcaDA'
+
+example = open('input.txt').read()
+#print(example)
+
+def removePairs(s):
+    temp = None
+    for i in range(len(s)-1):
+        c1 = s[i]
+        c2 = s[i+1]
+        if c1 == c2.lower() or c1.lower() == c2:
+            temp = s[:i] + s[i+2:]
+            return temp
+    if temp == None:
+        return s
+
+currString = example
+cont = True
+while cont:
+    if currString == removePairs(currString):
+        cont = False
+    else:
+        currString = removePairs(currString)
+
+print(currString)
